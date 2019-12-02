a5bedd477c74a3035e0c2a75fb541dda9be365d5
SeptemberHX <september_hx@outlook.com>
2019-12-02 17:03:26 +0800

    init

---
+++ src/main/java/com/sampleservice/xiechengservice/controller/MainController.java
@@ -0,0 +1,25 @@
+package com.sampleservice.xiechengservice.controller;
+
+import com.septemberhx.common.base.MResponse;
+import com.septemberhx.mclient.annotation.MApiFunction;
+import com.septemberhx.mclient.annotation.MRestApiType;
+import com.septemberhx.mclient.base.MObject;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author SeptemberHX
+ * @version 0.1
+ * @date 2019/11/25
+ */
+@RestController
+public class MainController extends MObject {
+    @ResponseBody
+    @RequestMapping(value = "/hotel", method = RequestMethod.POST)
+    @MRestApiType
+    @MApiFunction
+    public MResponse hotelFunction(@RequestBody MResponse requestData) {
+        MResponse result = new MResponse();
+        result.set("msg", "/hotel");
+        return result;
+    }
+}
